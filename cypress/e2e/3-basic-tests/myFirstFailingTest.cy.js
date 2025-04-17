97e29c079c9a6cb46f671b2d2e390ffafd61b6d2
Ramavtar-Nagar <ramavtarnagar13@gmail.com>
2025-04-17 16:21:08 +0530

    Adding Basic Test Files

---
+++ cypress/e2e/3-basic-tests/myFirstFailingTest.cy.js
@@ -0,0 +1,5 @@
+describe('My First Failing Test', () => {
+    it('Does not do much!', () => {
+        expect(true).to.equal(false)
+    });
+});
---
+++ cypress/e2e/4-cypress-steps/1-visitAPage.cy.js
@@ -0,0 +1,6 @@
+
+describe('Visit A Page', () => {
+    it('Visits The Kitchen Sink', () => {
+        cy.visit('https://example.cypress.io')
+    });
+});
