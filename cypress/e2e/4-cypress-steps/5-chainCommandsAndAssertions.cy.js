6a5782c31f858c56e8cb52333a01e7416d520b01
Ramavtar-Nagar <ramavtarnagar13@gmail.com>
2025-04-22 12:58:26 +0530

    Assertions

--- cypress/e2e/4-cypress-steps/5-chainCommandsAndAssertions.cy.js
+++ cypress/e2e/4-cypress-steps/5-chainCommandsAndAssertions.cy.js
@@ -1,3 +1,5 @@
+const { expect } = require("chai");
+
 // Adding more commands and assertions
 describe('Adding more commands and assertions', () => {
     it('Gets, Types and Asserts', () => {
@@ -17,3 +19,30 @@ describe('Adding more commands and assertions', () => {
 
     });
 });
+
+// ✅ Method 1: .should(...) ->
+
+cy.get('button').should('be.visible');
+cy.get('.error-msg').should('contain', 'Invalid Login');
+cy.get('input').should('have.value', 'Avatar')
+
+// ✅ Method 2: .then() + expect(...) (for more control)
+
+cy.get('.score').then(( $el ) => {
+    expect( $el.text()).to.equal('100')
+})
+
+// Chain multiple assertions:
+
+cy.get('button')
+    .should('be.visible')
+    .and('contain', 'submit')
+    .and('not.be.disabled')
+
+// ⚙️ 5. Real Example: Login Form
+
+    cy.get('[data-cy="username-input"]').type('avatar')
+    cy.get('[data-cy="password-input"]').type('789456333')
+    cy.get('[data-cy"submit-btn"]').click()
+    // After submit, check welcome message
+    cy.get('[data-cy="elcomw-msg"]').should('contain', 'welcome, Avatar')
